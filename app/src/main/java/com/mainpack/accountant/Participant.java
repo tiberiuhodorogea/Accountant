cd0a6b728f17dcfcccdeaec00a1e3965c4952765
Tiberiu Hodorogea <thodorogea@bitdefender.com>
2021-04-10 12:39:25 +0300

    further dev

--- app/src/main/java/com/mainpack/accountant/Participant.java
+++ app/src/main/java/com/mainpack/accountant/Participant.java
@@ -3,7 +3,6 @@ package com.mainpack.accountant;
 public class Participant {
     public String name;
     public double spent;
-    public boolean checked;
 
     public Participant(){
         name ="";
